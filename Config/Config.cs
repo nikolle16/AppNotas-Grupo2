fc9ebe233162509a6db80c245314a583bc7f8192
Victor Peralta <victorperalta70@yahoo.com>
2024-08-07 14:53:35 -0600

    Bugs Fixed

--- Config/Config.cs
+++ Config/Config.cs
@@ -8,19 +8,19 @@ namespace App_Notas___Grupo_2.Config
 {
     public class Config
     {
-        public static string EndPointCreate = "http://10.0.0.5:6000/api/user";
-        public static string EndPointList = "http://10.0.0.5:6000/api/user";
-        public static string EndPointUpdate = "http://10.0.0.5:6000/api/user";
-        public static string EndPointDelete = "http://10.0.0.5:6000/api/user";
-        public static string EndPointCreateNote = "http://10.0.0.5:6000/api/note";
-        public static string EndPointListNotes = "http://10.0.0.5:6000/api/note";
-        public static string EndPointGetNotes = "http://10.0.0.5:6000/api/note";
-        public static string EndPointGetNote = "http://10.0.0.5:6000/api/note";
-        public static string EndPointUpdateNote = "http://10.0.0.5:6000/api/note";
-        public static string EndPointDeleteNote = "http://10.0.0.5:6000/api/note";
-        public static string EndPointCreateAudio = "http://10.0.0.5:6000/api/audios";
-        public static string EndPointGetAudios = "http://10.0.0.5:6000/api/audios";
-        public static string EndPointDeleteAudio = "http://10.0.0.5:6000/api/audios";
+        public static string EndPointCreate = "http://192.168.0.3:6000/api/user";
+        public static string EndPointList = "http://192.168.0.3:6000/api/user";
+        public static string EndPointUpdate = "http://192.168.0.3:6000/api/user";
+        public static string EndPointDelete = "http://192.168.0.3:6000/api/user";
+        public static string EndPointCreateNote = "http://192.168.0.3:6000/api/note";
+        public static string EndPointListNotes = "http://192.168.0.3:6000/api/note";
+        public static string EndPointGetNotes = "http://192.168.0.3:6000/api/note";
+        public static string EndPointGetNote = "http://192.168.0.3:6000/api/note";
+        public static string EndPointUpdateNote = "http://192.168.0.3:6000/api/note";
+        public static string EndPointDeleteNote = "http://192.168.0.3:6000/api/note";
+        public static string EndPointCreateAudio = "http://192.168.0.3:6000/api/audios";
+        public static string EndPointGetAudios = "http://192.168.0.3:6000/api/audios";
+        public static string EndPointDeleteAudio = "http://192.168.0.3:6000/api/audios";
 
         public static string BearerToken = "brrr";
     }
